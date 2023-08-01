7d138647084c623fac2cb4ae42dacf2042fab87e
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-08-01 16:44:34 -0500

    Practica router DOM

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from './paginas/LandingPage';
+import FormularioLogin from './paginas/FormularioLogin';
+import Dashboard from './layout/Dashboard';
+import Login from "./layout/Login";
+import FundamentoUseEffect from "./paginas/FundamentoUseEffect";
+import FundamentoUseState from "./paginas/FundamentoUseState";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage />}></Route>
+
+        <Route path="/login" element={<Login />}>
+          <Route index element={<FormularioLogin />} />
+        </Route>
+
+        <Route path="/fundamentos" element={<Dashboard />}>
+          <Route path="usestate" element={<FundamentoUseState />} />
+          <Route path="useffect" element={<FundamentoUseEffect />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+export default App
